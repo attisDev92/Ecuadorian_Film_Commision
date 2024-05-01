bb0286a6f874b97b86cf3d4d0d7217c7885a8497
attisDev92 <attis.alejandro@gmail.com>
2024-05-01 17:24:44 -0500

    creacion de categorias

--- src/layouts/LocationGuide.jsx
+++ src/layouts/LocationGuide.jsx
@@ -2,4 +2,20 @@
 
 const LocationGuide = ({ text }) => {
 
+  return (
+    <>
+      <div>
+        <div></div>
+      </div>
+      <div>
+
+      </div>
+      <div>
+        
+      </div>
+    
+    </>
+  )
 }
+
+export default LocationGuide
