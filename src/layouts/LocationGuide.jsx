74414b489c8da563072bb3aa4666e2a8f8d74617
attisDev92 <attis.alejandro@gmail.com>
2024-05-02 11:18:43 -0500

    diseno de locaciones

--- src/layouts/LocationGuide.jsx
+++ src/layouts/LocationGuide.jsx
@@ -1,20 +1,72 @@
+import categoriesData from '../assets/data/data_categories.json'
+import imagesDataFull from '../assets/data/data_img_locations.json'
 
+import Section from '../components/Section/Section'
+import Category from '../components/Category/Category'
+import LocationCard from '../components/LocationCard/LocationCard'
+
+import { useState } from 'react'
+
+import styles from './LocationGuide.module.css'
 
 const LocationGuide = ({ text }) => {
 
-  return (
-    <>
-      <div>
-        <div></div>
-      </div>
-      <div>
+  const [ imagesNumber, setImagesNumber ] = useState(20)
 
+  const categories = text.idioma === 'esp' ? categoriesData.categorias_esp : categoriesData.categorias_eng
+
+  const imagesData = text.idioma == 'esp' ? imagesDataFull.español : imagesDataFull.english
+
+  const imagesLocations = imagesData.slice(0, imagesNumber)
+
+  const handleOnClickPlusImages = () => {
+    if (imagesNumber < imagesData.length) {
+      setImagesNumber(imagesNumber + 10)
+    }
+  }
+
+  const handleOnClickLessImages = () => {
+    if (imagesNumber > 20) {
+      setImagesNumber(imagesNumber - 10)
+    }
+  }
+
+
+  return (
+    <Section id={"locations_Guide"} text={text} title={text.LocationGuide.title}>
+      <div className={styles.categories}>
+        {
+          categories.map( category => (
+            <Category category={category} />
+          )) 
+        }
       </div>
-      <div>
-        
+      <div className={styles.location_cards}>
+        {
+          imagesLocations.map( image => (
+            <LocationCard image={image} />
+          ))
+        }
       </div>
-    
-    </>
+
+      {
+        imagesNumber < imagesData.length ? (
+        <button onClick={handleOnClickPlusImages}>Ver más</button>
+        ) : (
+          null
+        )
+      }
+
+      {
+        imagesNumber > 20 ? (
+          <button onClick={handleOnClickLessImages}>Ver menos</button>
+        ) : (
+          null
+        )
+      }
+      
+  
+    </Section>
   )
 }
 
