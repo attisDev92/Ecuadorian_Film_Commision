93255fd1b3f0196df65215635d199d56bae4db45
attisDev92 <attis.alejandro@gmail.com>
2024-05-08 11:03:27 -0500

    optimizacion de imagenes

--- src/components/VideoBanner/VideoBanner.jsx
+++ src/components/VideoBanner/VideoBanner.jsx
@@ -1,16 +1,14 @@
 import style from './VideoBanner.module.css'
-import imgBanner from '/assets/images/Still Reel CF.png'
 
 const VideoBanner = ({ text  }) => {
 
     return (
         <div className={style.container__video}>
-            <img src={imgBanner} alt="" />
-            {/* <video className={style.video}>
-                /* <source src={text.videoBanner.urlVideoMp4} type="video/mp4"/>
-                <source src={text.VideoBanner.urlVideoOgg} type="video/ogg"/>
+            <video className={style.video}  autoPlay loop preload='auto' >
+                <source src={`/assets/videos/${text.videoBanner.urlVideoMp4}`} type="video/mp4"/>
+                {/* <source src={text.VideoBanner.urlVideoOgg} type="video/ogg"/> */}
                 Tu navegador no soporta la etiqueta de 'video'.
-            </video> */}
+            </video>
         </div>
     )
 }
